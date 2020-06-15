f3a6a183d3ce9757bfaf679dc40e319ceadf8cd2
nonoyano <44346907+nonoyano@users.noreply.github.com>
2020-06-15 22:17:34 +0200

    Changed to libGDX/Gradle

---
+++ desktop/src/com/zombies/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zombies.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zombies.main.IsometricZombies;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IsometricZombies(), config);
+	}
+}
